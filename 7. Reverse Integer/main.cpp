4612d1a93dbe03ebc1844525b0c841a47b640c16
陈宇峰 <yufengcode@gmail.com>
2016-10-03 21:52:05 +0800

    bugfix

--- 7. Reverse Integer/main.cpp
+++ 7. Reverse Integer/main.cpp
@@ -16,26 +16,23 @@
 
 using namespace std;
 
+/**
+ *  32位整型表示的数字范围：-2147483648~2147483647
+ - 2^31 ~ 2^31-1
+ */
 int reverseNum(int x)
 {
-    int result;
+    long result;
     if (x >= 0)
     {
         ostringstream os;
         os << x;
         string strIn = os.str();
-
         std::reverse(strIn.begin(), strIn.end());
-
         istringstream is(strIn);
         is >> result;
-        // 判断是否溢出
-        // 把当前字符串继续进行逆转，和原先输入的字符串比较，相等表示没有溢出，否则表示溢出
-        int test = result;
-        ostringstream oss;
-        oss << test;
-        string testStr = oss.str();
-        if (strIn != testStr)
+
+        if (result > pow(2, 31) - 1)
         {
             // 溢出
             result = 0;
@@ -46,29 +43,24 @@ int reverseNum(int x)
         ostringstream os;
         os << -x;
         string strIn = os.str();
-
         std::reverse(strIn.begin(), strIn.end());
-
         istringstream is(strIn);
         is >> result;
-
         result = -result;
 
-        // 判断是否溢出
-        int temp = atoi(strIn.c_str());
-        if (result + temp != 0)
+        if (result < - pow(2, 31))
         {
             // 溢出
             result = 0;
         }
     }
 
-    return result;
+    return (int)result;
 }
 
 int main(int argc, const char * argv[])
 {
-    int result = reverseNum(10);
+    int result = reverseNum(32768);
     cout << result;
 
     return 0;
